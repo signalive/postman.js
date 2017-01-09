33f9d35adf043f233e5ef6c89f2d7bc5b7ddd070
Mert <mertdogar@gmail.com>
2017-01-09 16:59:42 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -11,15 +11,15 @@ declare module 'postman.js' {
     //     toJSON(): string;
     // }
 
-    class Client {
-        // constructor(contentWindow: Window, domain: string, timeout?: number);
-        // sendMessage(message: Message): void;
-        emit(name: string, payload?: any, callback?: (error: any, payload?: any) => void): void;
-        on(name: string, callback: (payload: any, done: (error?: any, payload?: any) => void) => void): void;
-        destroy(): void;
-    }
-
     namespace postman {
+        class Client {
+            // constructor(contentWindow: Window, domain: string, timeout?: number);
+            // sendMessage(message: Message): void;
+            emit(name: string, payload?: any, callback?: (error: any, payload?: any) => void): void;
+            on(name: string, callback: (payload: any, done: (error?: any, payload?: any) => void) => void): void;
+            destroy(): void;
+        }
+        
         function getClientByWindow(targetWindow: Window): Client;
         function createClient(targetWindow: Window, targetOrigin: string, opt_timeout?: number): Client;
     }
