fc3db368f7d27b668bc17d30e06a02e45f3e2356
Hazar Ilhan <hazarilhan@sabanciuniv.edu>
2019-10-07 13:48:51 +0300

    :bug: Add typings for timeout

--- index.d.ts
+++ index.d.ts
@@ -15,11 +15,11 @@ declare module 'postman.js' {
         class Client {
             // constructor(contentWindow: Window, domain: string, timeout?: number);
             // sendMessage(message: Message): void;
-            emit(name: string, payload?: any, callback?: (error: any, payload?: any) => void): void;
+            emit(name: string, payload?: any, callback?: (error: any, payload?: any) => void, timeout?: number): void;
             on(name: string, callback: (payload: any, done: (error?: any, payload?: any) => void) => void): void;
             destroy(): void;
         }
-        
+
         function getClientByWindow(targetWindow: Window): Client;
         function createClient(targetWindow: Window, targetOrigin: string, opt_timeout?: number): Client;
     }
